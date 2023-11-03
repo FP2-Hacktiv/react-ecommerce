35bba6e5462b170e549e2e59ebfdacc98d9d4a07
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-11-03 13:53:14 +0700

    feat: create all pages  and router

---
+++ src/pages/admin/dashboard/index.jsx
@@ -0,0 +1,5 @@
+const Page = () => {
+	return <div>Admin Dashboard Page</div>;
+};
+
+export default Page;
---
+++ src/pages/cart/index.jsx
@@ -0,0 +1,5 @@
+const Page = () => {
+	return <div>Cart Page</div>;
+};
+
+export default Page;
