604a60589bdfac882514c4ae16a9bc6e09cfdca8
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-11-10 20:24:04 +0700

    style: add link to detail product & edit button add to cart width to full and font size

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,100 +1,82 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchAllProduct } from "../../store/global/globalAction";
-import { addToCart, removeFromCart } from "../../store/global/globalSlice";
+import { addToCart } from "../../store/global/globalSlice";
 import troli from "../../assets/troli.svg";
 import Toast from "../../components/toast";
+import { Link } from "react-router-dom";
 
 const Page = () => {
-  const dispatch = useDispatch();
-  const { isLoading, products } = useSelector((state) => state.global);
-  const { isAuthenticated } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
+	const { isLoading, products } = useSelector((state) => state.global);
+	const { isAuthenticated } = useSelector((state) => state.auth);
 
-  const handleGetAllProduct = () => {
-    dispatch(fetchAllProduct());
-  };
+	const handleGetAllProduct = () => {
+		dispatch(fetchAllProduct());
+	};
 
-  const handleAddToCart = (product) => {
-    if (!isAuthenticated) {
-      return Toast({
-        type: "error",
-        message: "Login first to add product to your cart",
-      });
-    }
+	const handleAddToCart = (product) => {
+		if (!isAuthenticated) {
+			return Toast({
+				type: "error",
+				message: "Login first to add product to your cart",
+			});
+		}
 
-    dispatch(addToCart(product));
-    Toast({
-      type: "success",
-      message: `Success Add ${product.name} to Cart`,
-    });
-  };
+		dispatch(addToCart(product));
+		Toast({
+			type: "success",
+			message: `Success Add ${product.name} to Cart`,
+		});
+	};
 
-  const handleRemoveFromCart = (product) => dispatch(removeFromCart(product));
+	useEffect(() => {
+		handleGetAllProduct();
+	}, []);
 
-  useEffect(() => {
-    handleGetAllProduct();
-  }, []);
-
-  return (
-    <div>
-      {isLoading ? (
-        <h1>Loading....</h1>
-      ) : (
-        <div className="grid grid-cols-3 gap-10">
-          {products.map((item) => (
-            <div
-              className="mb-3 flex flex-col shadow-2xl border-solid border-2 border-slate-500 rounded-2xl px-4 gap-2 justify-evenly"
-              key={item._id}
-            >
-              <div className="flex flex-col gap-1">
-                <div className="pt-2 flex items-center justify-center h-56">
-                  <img className="h-full" src={item.image} alt={item.name} />
-                </div>
-                <p className="text-base">{item.name}</p>
-                <p>IDR {item.price.toLocaleString("en-US")}</p>
-              </div>
-              {!isAuthenticated ? (
-                <div className="flex flex-col gap-2 pb-4">
-                  <button
-                    className="flex flex-row items-center justify-between bg-[#87BCD9] rounded-2xl min-h-[3rem] w-56 text-left px-5"
-                    onClick={() => handleAddToCart(item)}
-                  >
-                    <p>Add Product</p>
-                    <img
-                      src={troli}
-                      className="h-8 w-8 bg-orange-400 rounded-full p-1"
-                    />
-                  </button>
-                </div>
-              ) : (
-                <>
-                  <div className="flex flex-col gap-2 pb-4">
-                    <button
-                      className="flex flex-row items-center justify-between bg-[#87BCD9] rounded-2xl min-h-[3rem] w-56 text-left px-5"
-                      onClick={() => handleAddToCart(item)}
-                    >
-                      <p>Add Product</p>
-                      <img
-                        src={troli}
-                        alt="Troli"
-                        className="h-8 w-8 bg-orange-400 rounded-full p-1"
-                      />
-                    </button>
-                    <button
-                      className="flex flex-row items-center justify-between rounded-2xl min-h-[3rem] w-56 text-left px-5 bg-red-500"
-                      onClick={() => handleRemoveFromCart(item)}
-                    >
-                      Remove Product
-                    </button>
-                  </div>
-                </>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{isLoading ? (
+				<h1>Loading....</h1>
+			) : (
+				<div className="grid grid-cols-3 gap-10">
+					{products.map((item) => (
+						<div
+							className="mb-3 flex flex-col shadow-2xl border-solid border-2 border-slate-500 rounded-2xl px-4 gap-2 justify-evenly"
+							key={item._id}>
+							<div className="flex flex-col">
+								<div className="pt-2 flex items-center justify-center h-56">
+									<img className="h-full" src={item.image} alt={item.name} />
+								</div>
+								<div className="my-2 h-[100px]">
+									<Link
+										to={`/detail/${item._id}`}
+										className="text-xl hover:text-blue-600 font-medium">
+										{item.name}
+									</Link>
+									<p className="text-lg font-semibold mt-3">
+										IDR {item.price.toLocaleString("en-US")}
+									</p>
+								</div>
+							</div>
+							<div className="flex  pb-4">
+								<button
+									className="flex flex-row items-center justify-between bg-[#87BCD9] hover:bg-blue-400 transition-colors rounded-2xl min-h-[3rem] w-full text-left px-5"
+									onClick={() => handleAddToCart(item)}>
+									<div className=""></div>
+									<p className="font-medium text-lg">Add To Cart</p>
+									<img
+										src={troli}
+										className="h-8 w-8 bg-orange-400 rounded-full p-1"
+									/>
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Page;
