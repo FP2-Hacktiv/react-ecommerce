1104e61597008fec196953e18817c1a8c8f4e014
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-11-11 13:07:50 +0700

    style: edit info when cart is empty

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import ShoppingCart from "../../components/cart-table";
+import { Link } from "react-router-dom";
 
 const Page = () => {
 	const { carts } = useSelector((state) => state.global);
@@ -7,8 +8,15 @@ const Page = () => {
 	return (
 		<div className="w-full">
 			{carts.length === 0 ? (
-				<div className="flex justify-center items-center">
-					<h1 className="font-semibold">No Cart</h1>
+				<div className="flex flex-col gap-3 justify-center items-center h-[calc(100vh-300px)]">
+					<h1 className="font-semibold text-3xl">
+						Ops... your shopping cart is still empty
+					</h1>
+					<Link
+						to="/"
+						className="py-3 px-5 bg-sky-900 font-semibold text-lg text-white rounded-md">
+						Shop Now
+					</Link>
 				</div>
 			) : (
 				<>
