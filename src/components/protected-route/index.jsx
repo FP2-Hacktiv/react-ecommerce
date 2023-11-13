72d15ba4a70a97b440258d322e364aeaf7a2a9f2
YusronFauzanN <Yusronsronz7@gmail.com>
2023-11-13 19:42:23 +0700

    protected auth

--- src/components/protected-route/index.jsx
+++ src/components/protected-route/index.jsx
@@ -4,29 +4,33 @@ import PropTypes from "prop-types";
 import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
-	const navigate = useNavigate();
-	const location = useLocation();
-	const currentPathname = location.pathname;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPathname = location.pathname;
 
-	const { user, isAuthenticated } = useSelector((state) => state.auth);
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
 
-	const isAdminRoute = currentPathname.includes("admin");
+  const isAdminRoute = currentPathname.includes("admin");
 
-	useEffect(() => {
-		if (!isAuthenticated) {
-			return navigate("/login");
-		}
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return navigate("/login");
+    }
 
-		if (isAdminRoute && isAuthenticated && !user.isAdmin) {
-			return navigate("/");
-		}
-	}, [isAuthenticated]);
+    if (isAuthenticated) {
+      return navigate("/");
+    }
 
-	return children;
+    if (isAdminRoute && isAuthenticated && !user.isAdmin) {
+      return navigate("/");
+    }
+  }, [isAuthenticated]);
+
+  return children;
 };
 
 ProtectedRoute.propTypes = {
-	children: PropTypes.element,
+  children: PropTypes.element,
 };
 
 export default ProtectedRoute;
